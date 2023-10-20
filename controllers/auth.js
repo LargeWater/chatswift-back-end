7d09f6352231ccbb2eb4c7353e4d67bbce144919
Nicholas Bailey <nickcyg1992@gmail.com>
2023-10-19 22:24:00 -0500

    remove profiles and cloudinary

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,34 +1,21 @@
 import jwt from 'jsonwebtoken'
 
 import { User } from '../models/user.js'
-import { Profile } from '../models/profile.js'
 
 async function signup(req, res) {
   try {
     if (!process.env.SECRET) throw new Error('no SECRET in back-end .env')
-    if (!process.env.CLOUDINARY_URL) {
-      throw new Error('no CLOUDINARY_URL in back-end .env file')
-    }
 
     const user = await User.findOne({ email: req.body.email })
     if (user) throw new Error('Account already exists')
 
-    const newProfile = await Profile.create(req.body)
-    req.body.profile = newProfile._id
+
     const newUser = await User.create(req.body)
 
     const token = createJWT(newUser)
     res.status(200).json({ token })
   } catch (err) {
     console.log(err)
-    try {
-      if (req.body.profile) {
-        await Profile.findByIdAndDelete(req.body.profile)
-      }
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ err: err.message })
-    }
     res.status(500).json({ err: err.message })
   }
 }
@@ -36,9 +23,6 @@ async function signup(req, res) {
 async function login(req, res) {
   try {
     if (!process.env.SECRET) throw new Error('no SECRET in back-end .env')
-    if (!process.env.CLOUDINARY_URL) {
-      throw new Error('no CLOUDINARY_URL in back-end .env')
-    }
 
     const user = await User.findOne({ email: req.body.email })
     if (!user) throw new Error('User not found')
