678a5a7a95437e0c00e703d1986f0a98b272616d
Nicholas Bailey <nickcyg1992@gmail.com>
2023-10-19 22:21:16 -0500

    remove profiles

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ import formData from 'express-form-data'
 import './config/database.js'
 
 // import routes
-import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
 
 // create the express app
@@ -22,7 +21,6 @@ app.use(express.json())
 app.use(formData.parse())
 
 // mount imported routes
-app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
 
 // handle 404 errors
